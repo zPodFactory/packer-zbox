b52fe77ad999d51b79766e2e73f8f482b1984e74
Timo Sugliani <timo.sugliani@gmail.com>
2023-02-28 17:47:08 +0100

    Move to GPG as apt-key is deprecated in Debian 11 & Double system disk for Docker containers
    Signed-off-by: Timo Sugliani <timo.sugliani@gmail.com>

--- scripts/zbox-apt.sh
+++ scripts/zbox-apt.sh
@@ -13,53 +13,52 @@ apt-get install -y \
   gnupg2 \
   software-properties-common
 
+# Create folder for all new added APT repositories GPG Signing Keys
+mkdir -m 0755 -p /etc/apt/keyrings
+
 ##
 ## Docker
 ## 
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 # Add Docker official repository
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" \
+| tee /etc/apt/sources.list.d/docker.list
 
 ##
 ## Hashicorp
 ## 
 
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+curl -fsSL https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /etc/apt/keyrings/hashicorp.gpg
 
-apt-add-repository \
-   "deb [arch=amd64] https://apt.releases.hashicorp.com \
-   $(lsb_release -cs) \
-   main"
+# Add Hashicorp official reposiroty
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/hashicorp.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" \
+| tee /etc/apt/sources.list.d/hashicorp.list
 
 ##
 ## Kubernetes 
 ## 
 
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /etc/apt/keyrings/kubernetes.gpg
 
 # Add Kubernetes official repository
-apt-add-repository \
-  "deb [arch=amd64] https://apt.kubernetes.io \
-  kubernetes-xenial \
-  main"
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/kubernetes.gpg] https://apt.kubernetes.io kubernetes-xenial main" \
+| tee /etc/apt/sources.list.d/kubernetes.list
 
 ##
 ## Powershell
 ##
 
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o /etc/apt/keyrings/microsoft.gpg
 
 # Add microsoft official repository
-add-apt-repository \
-   "deb [arch=amd64] https://packages.microsoft.com/debian/11/prod \
-   $(lsb_release -cs) \
-   main"
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/microsoft.gpg] https://packages.microsoft.com/debian/11/prod $(lsb_release -cs) main" \
+| tee /etc/apt/sources.list.d/microsoft.list
 
+
+# Update APT repository package list
 apt-get update
 
 echo '> Done'
