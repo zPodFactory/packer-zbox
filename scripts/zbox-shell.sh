61fd4621232b3072ccf55aa2691f6ac2cb87af48
Timo Sugliani <timo.sugliani@gmail.com>
2023-08-09 05:04:24 +0200

    Misc fixes
    Signed-off-by: Timo Sugliani <timo.sugliani@gmail.com>

--- scripts/zbox-shell.sh
+++ scripts/zbox-shell.sh
@@ -9,8 +9,8 @@ echo '> Installing zBox Shell...'
 
 apt-get install -y \
   zsh
-  
-echo '> Installing oh-my-zsh...'  
+
+echo '> Installing oh-my-zsh...'
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 echo 'alias ip="ip -c"' >> $HOME/.zshrc
@@ -20,11 +20,11 @@ echo 'alias diff="colordiff"' >> $HOME/.zshrc
 
 usermod --shell /bin/zsh root
 
-echo '> Installing oh-my-posh...'  
+echo '> Installing oh-my-posh...'
 wget -q https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
 chmod +x /usr/local/bin/oh-my-posh
 
-echo '> Installing posh themes...'  
+echo '> Installing posh themes...'
 mkdir $HOME/.poshthemes
 wget -q https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O $HOME/.poshthemes/themes.zip
 unzip $HOME/.poshthemes/themes.zip -d $HOME/.poshthemes
@@ -41,6 +41,8 @@ mkdir -vp $HOME/.cache
 
 # Only enable zbox theme on SSH, as it uses nerd fonts icons/symbols (https://www.nerdfonts.com).
 echo 'if [[ -n $SSH_CONNECTION ]]; then' >> $HOME/.zshrc
+echo '  alias ll="lsd -l"'  >> $HOME/.zshrc
+echo '  alias la="lsd -la"'  >> $HOME/.zshrc
 echo '  eval "$(oh-my-posh --init --shell zsh --config $HOME/.poshthemes/zbox.omp.json)"' >> $HOME/.zshrc
 echo 'fi' >> $HOME/.zshrc
 
--- scripts/zbox-system.sh
+++ scripts/zbox-system.sh
@@ -10,37 +10,25 @@ echo '> Installing System Utilities...'
 apt-get install -y \
   jq \
   bat \
+  duf \
   exa \
   fzf \
   git \
+  lsd \
   man \
   vim \
+  btop \
+  htop \
   make \
   ccze \
   tree \
   tmux \
-  htop \
   bzip2 \
   dstat \
   unzip \
   httpie \
-  colordiff
-  
-echo '> Installing duf...'
-# nice df alternative (duf): https://github.com/muesli/duf
-wget -q https://github.com/muesli/duf/releases/download/v0.8.1/duf_0.8.1_linux_amd64.deb -O /tmp/duf.deb
-dpkg -i /tmp/duf.deb 
-rm -vf /tmp/duf.deb
-
-echo '> Installing btop...'
-# nice top alternative (btop): https://github.com/aristocratos/btop
-mkdir /tmp/btop 
-wget -q https://github.com/aristocratos/btop/releases/download/v1.2.13/btop-x86_64-linux-musl.tbz -O /tmp/btop/btop.tbz
-
-cd /tmp/btop/
-tar -xvjf btop.tbz
-cd btop
-./install.sh
-rm -rf /tmp/btop
+  colordiff \
+  colortail \
+  syslog-ng
 
 echo '> Done'
