9306cd5ed44f1362747c9421adad2762582a035e
Timo Sugliani <timo.sugliani@gmail.com>
2025-09-09 11:45:43 +0000

    Switching from rc.local configuration to a proper systemd zbox-init unit

--- scripts/zbox-settings.sh
+++ scripts/zbox-settings.sh
@@ -5,7 +5,7 @@
 ## Misc configuration
 ##
 
-echo '> Debian Settings...'
+echo '> zBox Settings...'
 
 echo '> Installing resolvconf...'
 apt-get install -y resolvconf-admin
@@ -15,9 +15,9 @@ echo ""
 echo '> SSH directory'
 mkdir -vp $HOME/.ssh
 
-echo '> Debian acts as a Router now'
+echo '> zBox acts as a Router now'
 # Configure via sysctl.d drop-in (Debian may not ship /etc/sysctl.conf by default)
-cat > /etc/sysctl.d/99-zbox.conf << 'EOF'
+cat > /etc/sysctl.d/99-zbox.conf << EOF
 net.ipv4.ip_forward = 1
 net.ipv6.conf.all.disable_ipv6 = 1
 EOF
@@ -30,28 +30,11 @@ echo ">> zBox $(cat /etc/debian_version)" | tee -a /etc/issue /etc/issue.net > /
 echo ">>" | tee -a /etc/issue /etc/issue.net > /dev/null
 sed -i 's/#Banner none/Banner \/etc\/issue.net/g' /etc/ssh/sshd_config
 
-echo '> Enable rc.local facility for debian-init.py'
-cat << EOF > /etc/rc.local
-#!/bin/sh -e
-#
-# rc.local
-#
-# This script is executed at the end of each multiuser runlevel.
-# Make sure that the script will "exit 0" on success or any other
-# value on error.
-#
-# In order to enable or disable this script just change the execution
-# bits.
-#
-# By default this script does nothing.
-
-if [ ! -f /etc/zbox.config ]; then
-    /sbin/zbox-init.sh
-fi
-
-exit 0
-EOF
-chmod +x /etc/rc.local
+# Setup zbox-init.service for early first boot initialization (added from packer file copy)
+# This will detect if the VM should be configured with:
+# - cloud-init (if metadata or userdata is detected)
+# - OVF properties (if no cloud-init configuration is detected)
 systemctl daemon-reload
+systemctl enable zbox-init.service
 
 echo '> Done'
