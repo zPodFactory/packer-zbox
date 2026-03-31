c9e344d9a8550582c67b22ea62f52fbe4055ba26
Timo Sugliani <timo.sugliani@gmail.com>
2026-03-31 11:17:38 +0000

    Add chezmoi for dotfiles mgmt

--- scripts/zbox-system.sh
+++ scripts/zbox-system.sh
@@ -53,4 +53,16 @@ mv eza /usr/local/bin/eza
 #
 curl https://fx.wtf/install.sh | sh
 
+
+#
+# Install chezmoi (https://chezmoi.io/)
+# https://github.com/twpayne/chezmoi
+#
+curl -s https://api.github.com/repos/twpayne/chezmoi/releases/latest \
+| grep browser_download_url \
+| grep linux_amd64.deb \
+| cut -d '"' -f 4 \
+| xargs curl -LO \
+&& dpkg -i chezmoi_*_linux_amd64.deb && rm chezmoi_*_linux_amd64.deb
+
 echo '> Done'
